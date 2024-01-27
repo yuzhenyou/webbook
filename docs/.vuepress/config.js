b6f85680deb1a249c8f6e6a075e8528bda3f12f1
yuzhenyou <yuzhenyou@outlook.com>
2024-01-28 00:04:39 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
 export default defineUserConfig({
+    base: '/webbook/',
     bundler: viteBundler(),
     theme: defaultTheme({
         repoLabel: 'GitLab',
